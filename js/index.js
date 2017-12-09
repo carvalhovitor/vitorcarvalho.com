ad0c9b1140a5bcc567cb6036cd53ca09d6b47640
Vitor Carvalho <vdecarv@gmail.com>
2017-12-08 23:39:43 -0200

    More SCSS

--- js/index.js
+++ js/index.js
@@ -35,7 +35,6 @@ function loadResponsiveMenu() {
 }
 
 
-
 $(function() {
     
     setInterval(function() { 
@@ -45,7 +44,8 @@ $(function() {
     // Mobile menu
     
     $('.open-menu').on('click', function() {
-       	var $this = $(this);
+        var $this = $(this);
+           
 		$('#main-menu').toggleClass('full-height').find('> ul').slideToggle(600);
 		$this.toggleClass('rotate');
     })
@@ -58,11 +58,7 @@ $(function() {
     })
     
 
-    $('a[href*="#"]')
-    // Remove links that don't actually link to anything
-    .not('[href="#"]')
-    .not('[href="#0"]')
-    .click(function(event) {
+    $('a[href*="#"]').click(function(event) {
       // On-page links
       if (
         location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
@@ -70,8 +66,9 @@ $(function() {
         location.hostname == this.hostname
       ) {
         // Figure out element to scroll to
-        var $linkElem = $(this);
-        var target = $(this.hash);
+        var $linkElem = $(this),
+            target = $(this.hash);
+
         target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
         // Does a scroll target exist?
         if (target.length) {
